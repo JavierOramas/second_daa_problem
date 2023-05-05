1bb03eba8ed48f4b5d712a1beef2f507ec862525
Joramas <javiale2000@gmai.com>
2023-05-04 20:50:14 -0400

    divide by 0 error fixed

--- testcase_generator.py
+++ testcase_generator.py
@@ -7,25 +7,25 @@ from multiprocessing import Pool
 
 def generate_unitary_case(i):
     # to avoid same seed in multi-process threads
-    sleep(10/i)
+    sleep(10/(i+1)
     print(f"Generating test case no. {i}")
-    notes_length = np.random.randint(1, 25)
-    n = np.zeros(notes_length)
+    notes_length=np.random.randint(1, 25)
+    n=np.zeros(notes_length)
     for j in range(notes_length):
-        n[j] = np.random.randint(1, 100)
+        n[j]=np.random.randint(1, 100)
     return {'in': n, 'out': solve(n)}
 
 
 def generate(num):
 
-    tests = {}
+    tests={}
 
-    p = Pool(12)
+    p=Pool(12)
 
-    sols = p.map(generate_unitary_case, [i for i in range(num)])
+    sols=p.map(generate_unitary_case, [i for i in range(num)])
 
     for i in range(len(sols)):
-        tests[i] = sols[i]
+        tests[i]=sols[i]
 
     with open('tests.json', 'w') as f:
         json.dump(tests, f)
